c27001a66390f60e6ffa1a5fcf828d295e3c5a63
mohitsrma701 <mohitsrma7014@gmail.com>
2025-11-26 15:52:43 +0530

    new changes on 26 nov2

--- backend_server/settings.py
+++ backend_server/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # REQUIRED
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
